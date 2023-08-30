4a736746aef651bd80b5f05dad1502306a1bf751
Manuela Agudelo <manuela071997@gmail.com>
2023-08-30 20:24:08 +0200

    Tipos de datos y conceptos básicos

---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Strider';
+let hpPoints: number | string = 95;
+
+hpPoints = 'FULL';
+
+const isAlive:boolean = true;
+console.log({   // usando {} creamos un objeto
+    name, hpPoints, isAlive
+})
+
+export {};
